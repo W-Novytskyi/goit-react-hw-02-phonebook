f6c7370f12b79afa781ec95fea05a2a3548f436f
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-04-17 21:01:27 +0300

    початок 4 кроку

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,55 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
+import ContactForm from 'components/ContactForm';
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: '',
-    number: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleChange = event => {
-    const { name, value } = event.currentTarget;
-
-    this.setState({ [name]: value });
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
   };
 
-  onAddContact = () => {
-    const { contacts, name, number } = this.state;
-    const id = nanoid();
-
-    if (name !== '' && number !== '') {
-      const newContact = { id, name, number };
-      this.setState({ contacts: [...contacts, newContact] });
-      this.reset();
-    }
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
 
   render() {
-    const { contacts, name, number } = this.state;
+    const { filter } = this.state;
+    const filteredContacts = this.getFilteredContacts();
 
     return (
       <div>
         <h1>Phonebook</h1>
-        <form>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Number
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              value={number}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="button" onClick={this.onAddContact}>
-            Add contact
-          </button>
-        </form>
+        <ContactForm onAddContact={this.addContact} />
         <h2>Contacts</h2>
+        <div>
+          <label>
+            Find contacts by name
+            <input type="text" value={filter} onChange={this.changeFilter} />
+          </label>
+        </div>
         <ul>
-          {contacts.map(contact => (
+          {filteredContacts.map(contact => (
             <li key={contact.id}>
-              {contact.name}:{contact.number}
+              {contact.name}: {contact.number}
             </li>
           ))}
         </ul>
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// function ContactList({ filteredContacts }) {
+//   return (
+//     <ul>
+//       {this.props.filteredContacts.map(contact => (
+//         <li key={contact.id}>
+//           {contact.name}: {contact.number}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export default ContactList;
